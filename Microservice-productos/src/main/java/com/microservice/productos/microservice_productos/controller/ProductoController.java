c0244f441eb61cb2f7793bf2808e92ab9f7422ad
izaivanegas <62458337+izaivanegas@users.noreply.github.com>
2025-05-27 22:08:41 -0600

    Se agrega la creacion de un producto

--- Microservice-productos/src/main/java/com/microservice/productos/microservice_productos/controller/ProductoController.java
+++ Microservice-productos/src/main/java/com/microservice/productos/microservice_productos/controller/ProductoController.java
@@ -4,11 +4,11 @@ import com.microservice.productos.microservice_productos.model.Producto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.microservice.productos.microservice_productos.service.ProductoService;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Data
@@ -21,10 +21,25 @@ public class ProductoController {
     private final ProductoService productoService;
 
 
-    @GetMapping("/all")
+    @GetMapping("/todos")
     public ResponseEntity<List<Producto>> getAllProductos(){
         return ResponseEntity.ok(this.productoService.getAllProductos());
     }
+
+
+    @PostMapping("nuevo")
+    public ResponseEntity<Producto> crearProducto(@RequestBody Producto producto, UriComponentsBuilder ucb){
+
+        URI uriEmpleado = ucb
+                .path("/producto/nuevo/{id}")
+                .buildAndExpand(this.productoService.crearProducto(producto).getId())
+                .toUri();
+
+        return ResponseEntity.created(uriEmpleado).build();
+    }
+
+
+
 
 
 
